f38b7f7ba8c19124a71adf0038790110c4503da3
In-VIN-sible <alexsenchuk2013@gmail.com>
2025-08-10 22:32:33 +0300

    Add files via upload
    js 1

---
+++ lang.js
@@ -0,0 +1,44 @@
+const langArr = {
+    'title': {
+        'uk': 'Мультимовна сторінка',
+        'en': 'Multiligual page',
+        'fr':'Page multilingue'
+    },
+'text': {
+    'uk': 'Це моя тестова сторінка',
+     'en': 'This is my test page',
+      'fr': 'Ceci est ma page de test.'
+},
+'name': {
+    'uk': 'Ноутбук Lenovo IdePad',
+     'en': 'laptop Lenovo IdePad',
+     'fr': 'Ordinateur portable Lenovo IdePad'
+},
+
+'os': {
+    'uk': 'Операційна система',
+     'en': 'Operation system',
+      'fr': 'Système opérateur'
+},
+'memory': {
+    'uk': "Об'єм пам'яті",
+     'en': 'Memory capacity',
+      'fr':'Capacité mémoire'
+},
+'type': {
+    'uk': "Тип пам'яті",
+     'en': 'Memory type',
+      'fr': 'Type de mémoire'
+},
+    'color': {
+        'uk': 'Колір',
+        'en': 'Color',
+        'fr': 'Couleur',
+    },
+    'cruas': {
+        'uk': 'Люблю французькі круасанчики',
+        'en': 'I love French croissants',
+        'fr': "J'adore les croissants français",
+    }
+
+}
